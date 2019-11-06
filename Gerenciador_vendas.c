e58f98a5c81f687adfaad39b1b69c140be831fb4
JuniorJWJ <54831275+JuniorJWJ@users.noreply.github.com>
2019-11-05 22:03:06 -0300

    Erro na matriz

--- Gerenciador_vendas.c
+++ Gerenciador_vendas.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/*int stringToInteger(char string[]){
-    int i, digit, j = 1, m =0 ;
-
-    for(  i = strlen(string)-1 ; i>=0 ; i--){
-        digit = string[i];
-        digit = digit - 48;
-        m = m + (digit * j);
-        j = j * 10;
-    }
-    return m;
-}*/
+#include <conio.h>
+#include <string.h>
 
 int main(){
 
@@ -20,44 +10,71 @@ int main(){
   char Linha[100];
   char *result;
   int produto_dia[10][7];
-  int i, produto = 0, dia = 0;
-  arq = fopen("Entrada.txt", "rt");       // LEITURA
+  char nome_produto[11][20] = {"Farinha",
+                               "Arroz",
+                               "Feijao",
+                               "Frango",
+                               "Carne",
+                               "Sabonete",
+                               "Escova",
+                               "Pasta",
+                               "Shampoo",
+                               "Condicionador"
+                              };
+  float preco_produto[10];
+  int i,j = 0, produto = 0, dia = 0, linha_preco =0 ;
+  arq = fopen("Entrada.txt", "rt");             // LEITURA
   arq1 = fopen("Relatorio (saida).txt", "wt");  // GRAVAÇÃO
 
-  if (arq == NULL)  // ERRO NA ABERTURA
-  {
+  if (arq == NULL){  // ERRO NA ABERTURA
      printf("Problemas na abertura do arquivo\n");
      return;
   }
 
   i = 1;
 
+  printf("\n%s", nome_produto[0]);
   while (!feof(arq)){
-	// Lê uma linha (inclusive com o '\n')
-      result = fgets(Linha, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n']
+  printf("\n%s", nome_produto[0]);
+      // LÊ UMA LINHA (INCLUSIVE COM O '\N')
+      result = fgets(Linha, 100, arq);  // O 'FGETS' LÊ ATÉ 99 CARACTÉRES OU ATÉ O '\N']
 
-      if (result)  // Se foi possível ler
-	  printf("Linha %d : %s",i,Linha);
-	  result = fprintf(arq1,"Linha %d\n",i);
-	  produto_dia[produto][dia] = atoi(Linha);
-      i++;
-      dia++;
-      if(dia>6){
-        dia=0;
-        produto++;
+      if (result){  // SE FOI POSSÍVEL LER
+          printf("Linha %d : %s",i,Linha);
+          result = fprintf(arq1,"Linha %d\n",i);
+          produto_dia[produto][dia] = atoi(Linha);
+
+          i++;
+          dia++;
+
+          if(dia>6){
+            dia=0;
+            produto++;
+          }
+          if(i>70){
+            preco_produto[linha_preco]=atoi(Linha);
+            linha_preco++;
+          }
       }
   }
 
+  printf("\n%s", nome_produto[0]);
   for( produto = 0; produto < 10 ; produto++){
     printf("\n");
-
     for( dia = 0; dia < 7 ; dia++){
-        printf("%i  ",produto_dia[produto][dia]); //= stringToInteger(Linha);
+        printf("%i  ",produto_dia[produto][dia]);
     }
-
   }
 
+  for(j=0; j<11; j++){
+        printf("\n%s", nome_produto[j]);
+  }
+
+
+
+  printf("\n%s", nome_produto[0]);
   fclose(arq);
 
+    getchar();
 	return 0;
 }
